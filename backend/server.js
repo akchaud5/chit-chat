7cd8babc238fd73f50e8f64f94699de7cb27e1d6
akchaud5 <Ayushkumar.chaudhary2003@gmail.com>
2025-03-01 21:40:07 -0700

    preparing for render

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.NODE_ENV === "production" ? "*" : "http://localhost:3000",
     // credentials: true,
   },
 });
